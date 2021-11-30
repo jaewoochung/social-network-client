a2f5726d983ef35e08250bc46b99e82a65cd50dc
Jaewoo Chung <jaewoochung77@gmail.com>
2021-11-29 22:12:21 -0500

    added proxy capabilities

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api',
-        destination: 'http://localhost:3001/'
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/:path*'
       }
     ]
   }
